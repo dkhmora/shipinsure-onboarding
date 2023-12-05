af7f4bd65b9ecd528918b2814aa58c5680572b16
Daryl Kiel Mora <dkhmora@gmail.com>
2023-12-04 16:41:51 -0800

    Added sidebar header image

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,6 +1,16 @@
 import React from "react";
+import SideBarHeaderImage from "../assets/images/sidebar-header.png";
 import "./SideBar.css";
 
 export default function SideBar() {
-  return <div class="sidebar-container">SideBar</div>;
+  return (
+    <div class="sidebar-container">
+      <img
+        src={SideBarHeaderImage}
+        class="sidebar-header-image"
+        alt="sidebar-header"
+      />
+      Sidebar
+    </div>
+  );
 }
