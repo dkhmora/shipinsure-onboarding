5d6e465588a7afa8b63a501a4da0ddf0a6e608cc
Daryl Kiel Mora <dkhmora@gmail.com>
2023-12-04 17:26:04 -0800

    Added sidebar steps

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import "./SideBar.css";
 import Header from "./Header";
+import Steps from "./Steps";
 
 export default function SideBar() {
   return (
     <div class="sidebar-container">
       <Header />
-      Sidebar
+      <Steps />
     </div>
   );
 }
---
+++ src/components/SideBar/Steps.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Steps() {
+  return (
+    <div class="steps-container">
+      <div class="selected-step-text">Personal Info</div>
+
+      <div class="default-step-text">Monthly Orders</div>
+
+      <div class="default-step-text">Billing</div>
+    </div>
+  );
+}
