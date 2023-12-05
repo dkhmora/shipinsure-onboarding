04e6e546194080f69199ac37f6b0377c1087e18f
Daryl Kiel Mora <dkhmora@gmail.com>
2023-12-05 00:00:28 -0800

    Added Card Number field

--- src/components/Form/BillingForm.jsx
+++ src/components/Form/BillingForm.jsx
@@ -1,16 +1,61 @@
 import React, { useState } from "react";
 import PlaidIcon from "../../assets/icons/PlaidLogo.svg";
 import "./Form.css";
+import TextInput from "./TextInput";
 
 export default function BillingForm() {
+  const [formData, setFormData] = useState({
+    storeName: "",
+    fullName: "",
+    email: "",
+    storeUrl: "",
+  });
+
+  const { storeName, fullName, email, storeUrl } = formData;
+
+  const handleClickUpdate = (e, label) => {
+    console.log(e);
+  };
+
+  const handleInputChange = (fieldName, value) => {
+    setFormData((prevData) => ({
+      ...prevData,
+      [fieldName]: value,
+    }));
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("Form submitted:", e);
+    console.log("Form submitted:", formData);
   };
 
   const validateForm = () => {
-    return null;
+    const errors = {};
+
+    // Basic required validation
+    if (!storeName.trim()) {
+      errors.storeName = "Store Name is required";
+    }
+
+    if (!fullName.trim()) {
+      errors.fullName = "Full Name is required";
+    }
+
+    if (!email.trim()) {
+      errors.email = "Email is required";
+    } else if (!email.includes("@")) {
+      errors.email = "Invalid email format";
+    }
+
+    if (!storeUrl.trim()) {
+      errors.storeUrl = "Store URL is required";
+    }
+
+    return errors;
   };
+
+  const errors = validateForm();
+  const formValid = Object.keys(errors).length === 0;
 
   return (
     <section class="form-container">
@@ -26,14 +71,24 @@ export default function BillingForm() {
       </p>
 
       <form className="form-inputs-container" onSubmit={handleSubmit}>
-        <section className="form-inputs-section"></section>
+        <section className="form-inputs-section">
+          <TextInput
+            label="Card Number"
+            value={storeName}
+            placeholder="1111 2222 3333 4444"
+            onChange={(value) => handleInputChange("storeName", value)}
+            required
+            textInputClassName="text-input"
+            hasError={errors.email}
+          />
+        </section>
 
         <button
           className={`form-submit-button ${
-            !validateForm() ? "disabled-form-submit-button" : ""
+            !formValid ? "disabled-form-submit-button" : ""
           }`}
           type="submit"
-          disabled={!validateForm()}
+          disabled={formValid}
         >
           Next
         </button>
