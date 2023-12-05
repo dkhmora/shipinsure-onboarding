d4d7e7b8daf2a77c5ce736e0c601c68c1cc67b03
Daryl Kiel Mora <dkhmora@gmail.com>
2023-12-04 21:25:54 -0800

    Added personal info form states

--- src/components/Form/PersonalInfoForm.jsx
+++ src/components/Form/PersonalInfoForm.jsx
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Container.css";
 import TextInput from "./TextInput";
 
 export default function PersonalInfoForm() {
+  const [formData, setFormData] = useState({
+    storeName: "",
+    fullName: "",
+    email: "",
+    storeUrl: "",
+  });
+
   const handleClickUpdate = (e, label) => {
     console.log(e);
   };
+
+  const handleInputChange = (fieldName, value) => {
+    setFormData((prevData) => ({
+      ...prevData,
+      [fieldName]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Form submitted:", formData);
+  };
+
+  const { storeName, fullName, email, storeUrl } = formData;
 
   return (
     <section class="form-container">
@@ -15,22 +36,28 @@ export default function PersonalInfoForm() {
         Let's make this easy--Please verify the information below is correct.
       </p>
 
-      <section className="form-inputs-container">
+      <form className="form-inputs-container" onSubmit={handleSubmit}>
         <section className="form-inputs-section">
           <TextInput
             label="Store Name"
+            value={storeName}
+            onChange={(value) => handleInputChange("storeName", value)}
             required
             textInputClassName="grey-text-input"
           />
 
           <TextInput
             label="Full Name"
+            value={fullName}
+            onChange={(value) => handleInputChange("fullName", value)}
             required
             textInputClassName="grey-text-input"
           />
 
           <TextInput
             label="Email Address"
+            value={email}
+            onChange={(value) => handleInputChange("email", value)}
             required
             textInputClassName="text-input"
             onClickUpdate={handleClickUpdate}
@@ -38,14 +65,18 @@ export default function PersonalInfoForm() {
 
           <TextInput
             label="Store URL"
+            value={storeUrl}
+            onChange={(value) => handleInputChange("storeUrl", value)}
             required
             textInputClassName="text-input"
             onClickUpdate={handleClickUpdate}
           />
         </section>
 
-        <button className="main-button">Confirm</button>
-      </section>
+        <button className="main-button" type="submit">
+          Confirm
+        </button>
+      </form>
     </section>
   );
 }
