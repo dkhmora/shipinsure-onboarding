1bdd63fd4dafadc9c470a761be9ec73f15329278
Daryl Kiel Mora <dkhmora@gmail.com>
2023-12-04 18:15:29 -0800

    Created form container component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
 import SideBar from "./components/SideBar/SideBar";
+import FormContainer from "./components/Form/Container";
 
 function App() {
   return (
     <div className="App">
       <SideBar />
+
+      <FormContainer />
     </div>
   );
 }
