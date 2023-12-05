a92c71990c910bf8b1a7d7e64b56e17e4387c9db
Daryl Kiel Mora <dkhmora@gmail.com>
2023-12-05 01:51:57 -0800

    Renamed SideBar component folder to NavBar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import SideBar from "./components/SideBar/SideBar";
+import SideBar from "./components/NavBar/SideBar";
 import FormContainer from "./components/Form/Container";
 import { useState } from "react";
 import { onboardingSteps } from "./constants";
