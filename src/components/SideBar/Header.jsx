81f8de96262403ba3c323c1b0b4d66500562074d
Daryl Kiel Mora <dkhmora@gmail.com>
2023-12-04 17:14:11 -0800

    Added text to sidebar header

--- src/components/SideBar/Header.jsx
+++ src/components/SideBar/Header.jsx
@@ -4,12 +4,16 @@ import "./SideBar.css";
 
 export default function SideBarHeader() {
   return (
-    <div>
-      <img
-        src={SideBarHeaderImage}
-        class="sidebar-header-image"
-        alt="sidebar-header"
-      />
+    <div className="sidebar-header-container">
+      <div className="sidebar-header-image">
+        <img src={SideBarHeaderImage} alt="sidebar-header" />
+      </div>
+
+      <div className="sidebar-header-text-overlay">
+        <div className="sidebar-header-text">Onboarding</div>
+
+        <div className="sidebar-header-sub-text">0/3 steps complete!</div>
+      </div>
     </div>
   );
 }
