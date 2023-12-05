7db1697f0095bdc9199fb40eb245b61eb0541b08
Daryl Kiel Mora <dkhmora@gmail.com>
2023-12-04 17:36:09 -0800

    Added sidebar icons

--- src/components/SideBar/Steps.jsx
+++ src/components/SideBar/Steps.jsx
@@ -1,13 +1,27 @@
 import React from "react";
+import RadioIcon from "../../assets/icons/Radio.svg";
+import CheckIcon from "../../assets/icons/Check.svg";
 
 export default function Steps() {
   return (
     <div class="steps-container">
-      <div class="selected-step-text">Personal Info</div>
+      <div class="step-text-container">
+        <img src={RadioIcon} class="step-icon" alt="radio" />
 
-      <div class="default-step-text">Monthly Orders</div>
+        <div class="selected-step-text">Personal Info</div>
+      </div>
 
-      <div class="default-step-text">Billing</div>
+      <div class="step-text-container">
+        <img src={RadioIcon} class="step-icon" alt="radio" />
+
+        <div class="default-step-text">Monthly Orders</div>
+      </div>
+
+      <div class="step-text-container">
+        <img src={RadioIcon} class="step-icon" alt="radio" />
+
+        <div class="default-step-text">Billing</div>
+      </div>
     </div>
   );
 }
