e5436cbe3c44783ef15f976e0010e2073f3d3606
Daryl Kiel Mora <dkhmora@gmail.com>
2023-12-04 20:01:32 -0800

    Moved reviews section to personal info form

--- src/components/Form/Container.jsx
+++ src/components/Form/Container.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "./Container.css";
 import PersonalInfoForm from "./PersonalInfoForm";
+import ReviewSection from "./ReviewSection";
 
 export default function Container() {
   return (
     <div className="main-container">
       <PersonalInfoForm />
+
+      <ReviewSection />
     </div>
   );
 }
--- src/components/Form/PersonalInfoForm.jsx
+++ src/components/Form/PersonalInfoForm.jsx
@@ -25,8 +25,6 @@ export default function PersonalInfoForm() {
 
         <button className="main-button">Confirm</button>
       </section>
-
-      <ReviewSection />
     </section>
   );
 }
