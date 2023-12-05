5f05eba595834f39f11119e71b9038fdde729c75
Daryl Kiel Mora <dkhmora@gmail.com>
2023-12-04 19:04:15 -0800

    Added required asterisk for required fields

--- src/components/Form/PersonalInfoForm.jsx
+++ src/components/Form/PersonalInfoForm.jsx
@@ -12,7 +12,13 @@ export default function PersonalInfoForm() {
       </p>
 
       <section>
-        <TextInput label="Store Name" />
+        <TextInput label="Store Name" required />
+
+        <TextInput label="Full Name" required />
+
+        <TextInput label="Email address" required />
+
+        <TextInput label="Store URL" required />
       </section>
     </section>
   );
--- src/components/Form/TextInput.jsx
+++ src/components/Form/TextInput.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import "./Container.css";
 
-export default function TextInput({ label, value, onChange, placeholder }) {
+export default function TextInput({
+  label,
+  value,
+  onChange,
+  placeholder,
+  required,
+}) {
   return (
     <section className="text-input-container">
-      <p className="text-input-label">{label}</p>
+      <p className="text-input-label">
+        {label}
+        {required ? <span className="required-asterisk">*</span> : null}
+      </p>
 
       <input
         type="text"
